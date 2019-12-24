7512d56da08c0e2a17b58ed099efc403ffa66204
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-12-25 04:40:00 +0900

    スクリプトはscripts/に

---
+++ scripts/ArrayQueue.ts
@@ -0,0 +1,18 @@
+import ArrayQueue from "../ArrayQueue";
+
+const a = new ArrayQueue<number>();
+
+a.add(0);
+console.log(a);
+a.add(1);
+console.log(a);
+a.add(2);
+console.log(a);
+a.add(3);
+console.log(a);
+
+a.remove();
+console.log(a);
+
+a.add(4);
+console.log(a);

56e6cefba2758e4678b64167d070240c9dac3681
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-01-14 05:56:24 +0900

    [p55] 3.1 SLList: 単方向連結リスト

---
+++ src/SLList.ts
@@ -0,0 +1,57 @@
+class SLListNode<T> {
+  next: SLListNode<T> | null;
+  x: T
+
+  constructor(x: T) {
+    this.x = x;
+    this.next = null;
+  }
+}
+
+
+export default class SLList<T> {
+  head: SLListNode<T> | null;
+  tail: SLListNode<T> | null;
+  n: number;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.n = 0;
+  }
+
+  push(x: T) {
+    const u = new SLListNode(x);
+    u.next = this.head;
+    this.head = u;
+    if (this.n === 0) {
+      this.tail = u;
+    }
+    this.n++;
+    this.log();
+    return x;
+  }
+
+  pop() {
+    if (this.n === 0) return null;
+    const u = this.head!;
+    const x = u.x;
+    this.head = u.next;
+    this.n--;
+    if (this.n === 0) {
+      this.tail = null;
+    }
+    this.log();
+    return x;
+  }
+
+  log() {
+    const data = [];
+    let current = this.head;
+    while(current) {
+      data.push(current.x);
+      current = current.next;
+    }
+    console.log(data.join(', '));
+  }
+}
